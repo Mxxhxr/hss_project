6ad4795caac1c99cf8f0fe841b43e2854e391550
Maahir Vohra <117885945+Mxxhxr@users.noreply.github.com>
2024-12-04 22:41:09 -0500

    updates

--- src/components/StatePage.jsx
+++ src/components/StatePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import '../components/StatePage.css';
+import './StatePage.css';
 
 const StatePage = () => {
   const { stateName } = useParams(); 
