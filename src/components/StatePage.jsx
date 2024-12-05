5a67eee3841a1dd0b27a1c1351bf12a87669ec67
Maahir Vohra <117885945+Mxxhxr@users.noreply.github.com>
2024-12-05 18:38:22 -0500

    fixed image paths

--- src/components/StatePage.jsx
+++ src/components/StatePage.jsx
@@ -13,7 +13,7 @@ const StatePage = () => {
       image: 'andaman_image.jpg', // Replace with actual image path
     },
     'Gujarat': {
-        images: ['../src/images/gujarat.jpg'],
+        images: ['/images/Gujarat.jpg'],
         description: `A state in western India, where my parents are from.\n
         Its most popular/well known dish is Dhokla. Dhokla is a light, fluffy, spongy, and savory cake. It is made from a fermneted batter made from chickpea and rice flour that is steamed.\n
         Dhokla's history is traced back to Gujarats vegetarian culture and its emphasis on fermented foods. Fermentation not only enhances the nutritional value of the food 
@@ -31,7 +31,7 @@ const StatePage = () => {
         https://www.indianhealthyrecipes.com/khaman-dhokla/`,
     },
     'Kerala': {
-        images: ['../src/images/kerala_land.webp', '../src/images/kerala.jpg'], // Array of image paths
+        images: ['/images/Kerala_land.webp', '/images/Kerala.jpg'], // Array of image paths
 
         description:`A state in southern India known for its tropical climate, palm-lined beaches and backwaters. Inland are the Western Ghats, whose mountains support tea, coffee and spice plantations. Filled with wildlife, national parks, and sanctuaries, Kerala is a big tourist attraction in India.
         
@@ -46,7 +46,7 @@ const StatePage = () => {
         https://www.whiskaffair.com/kerala-parotta-recipe/`,
     },
     'Rajasthan': {
-      images: ['../src/images/rajasthan.jpg'],
+      images: ['/images/Rajasthan.jpg'],
       description: `Sitting above Gujarat is Rajasthan. With little availability of fresh fruits and vegetables, their cuisine distinguishes them from the rest.
 
       A few common ingredients are beans, lentils, gram flour, corn, barley, millet, bajra, bread, and dairy products like Ghee (purified butter). The general cuisine is mostly vegetarian, but there are some meat based dishes. Rajasthan is famous for its Dal Bati Churma
@@ -54,20 +54,20 @@ const StatePage = () => {
       Dal Bati Churma is a traditional, tasty meal made as a combo of savoury dal (lentils)l, deep fried wheat dough (bati) and crumbled, sweetened & flavoured bati (churma).`
     },
     'Haryana': {
-      images: ['../src/images/haryana.jpg'],
+      images: ['/images/Haryana.jpg'],
       description: `Haryana is a state in northern India. It is known for its agriculture and dairy products. The state is known for its rich culture and heritage.
       The state has a rich history and is known for its Ker Sangri, which is made with dried Ker berries and Sangri beans. The ker berries are native to Haryana. The dish is usually eaten with roti, which is very commong in India.`
     },
     // Add more states and their info here
     'Punjab': {
-      images: ['../src/images/punjab.jpg'],
+      images: ['/images/Punjab.jpg'],
       description: `Punjab is a state in northern India. The state is known for its rich culture and heritage.
       The state is known for its Sarson ka Saag, among many other things. This is a seasonal dish mostly in the winter. In India, they have seasonal dishes, using whatever is in season, and it makes for some great food.
       
       Sarson ka Saag is a dish made from mustard greens and spices. It also is eaten with roti. It is very nutrious and is a great source of vitamins and minerals.`
     },
     'Jammu & Kashmir': {
-      images: ['../src/images/kashmir.jpg'],
+      images: ['/images/Kashmir.jpg'],
       description: `Kashmir is a region in northern India known for its stunning natural beauty, including the Himalayan mountain range, lush valleys, and beautifal lakes with mountainous backgrounds.
 
       Their most popular dish is Rogan Josh. This is a type of curry made with  lamb cooked in a sauce made from garlic, yogurt, and various spices. The dish uses dried Kashmiri chilies.
@@ -80,7 +80,7 @@ const StatePage = () => {
       https://www.bbcgoodfood.com/recipes/lamb-rogan-josh`
     },
     'Ladakh': {
-      images: ['../src/images/ladakh.webp'],
+      images: ['/images/Ladakh.webp'],
       description: `Ladakh is another region in north India known for its stunning landscapes, mountains, high altitude deserts, and monasteries.
 
       One of the most popular dishes in Ladakh is Thukpa, a noodle soup that is hearty and warming, perfect for the cold climate of the region. Thukpa is made with noodles, vegetables, and sometimes meat.
@@ -92,7 +92,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/thukpa-recipe-veg-thukpa/`
     },
     'Uttar Pradesh': {
-      images: ['../src/images/uttar_pradesh.jpg'],
+      images: ['/images/Uttar_pradesh.jpg'],
       description: `Uttar Pradesh is a state in northern India known for its historical monuments, including the Taj Mahal.
 
       One of the most popular dishes in Uttar Pradesh is the Tunday Kebab, a kebab made from minced meat and a blend of spices. The dish is known for its soft texture and rich flavor.
@@ -104,7 +104,7 @@ const StatePage = () => {
       https://www.cookingwithsiddhi.com/tunday-kababi-recipe/`
     },
     'Madhya Pradesh': {
-      images: ['../src/images/madhya_pradesh.jpg'],
+      images: ['/images/Madhya_pradesh.jpg'],
       description: `A state in central India known for its rich heritage, and natural beauty.
 
       One of the most popular dishes in Madhya Pradesh is Poha. My mom ahas been making this since I was a kid and at one point it was my favorite thing to eat for breakfast.
@@ -116,7 +116,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/poha-recipe-kanda-poha/`
     },
     'Maharashtra': {
-      images: ['../src/images/Maharashtra.jpg'],
+      images: ['/images/Maharashtra.jpg'],
       description: `A state in western India known for its colorful culture, history, and cuisine.
 
       One of the most popular dishes in Maharashtra is Vada Pav, a street food snack made from a potato patty that is deep-fried served in a bun with multiple chutneys.
@@ -128,7 +128,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/vada-pav-how-to-make-wada-pav/`
     },
     'Karnataka': {
-      images: ['../src/images/karnataka.jpg'],
+      images: ['/images/Karnataka.jpg'],
       description: `Karnataka is a state in south west India known for its diverse culture.
 
       One of the most popular dishes in Karnataka is Bisi Bele Bath, a spicy rice dish made with lentils, vegetables, and unique spices.
@@ -140,7 +140,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/bisi-bele-bath-recipe/`
     },
     'Tamil Nadu': {
-      images: ['../src/images/tamil_nadu.webp'],
+      images: ['/images/Tamil_nadu.webp'],
       description: `Tamil Nadu is a state in southern India known for its intricate Dravidian style Hindu temples and ancient history (like much of India).
 
       One of the most popular dishes in Tamil Nadu is Dosa, which is a thin, crispy "pancake". The batter is similar to crepes, which is very wet, although this is made from fermented rice and lentils.
@@ -152,7 +152,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/dosa-recipe-dosa-batter-recipe/`
     },
     'Andhra Pradesh': {
-      images: ['../src/images/andhra_pradesh.jpg'],
+      images: ['/images/Andhra_pradesh.jpg'],
       description: `A state in southeastern India known for its rich cultural heritage and spicy cuisine.
 
       One of the most popular dishes in Andhra Pradesh is Hyderabadi Biryani, a famous rice dish made with basmati rice, meat (usually chicken or mutton), and a blend of aromatic spices.
@@ -164,7 +164,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/hyderabadi-biryani-recipe/`
     },
     'Telangana': {
-      images: ['../src/images/telangana.webp'],
+      images: ['/images/Telangana.webp'],
       description: `Telangana is a state in southern India known for its rich history, culture, and cuisine.
 
       One of the most popular dishes in Telangana is Pachi Pulusu, a traditional tamarind-based stew made with raw mangoes, onions, and spices.
@@ -174,7 +174,7 @@ const StatePage = () => {
 
     },
     'Chhattisgarh': {
-      images: ['../src/images/chhattisgarh.jpg'],
+      images: ['/images/Chhattisgarh.jpg'],
       description: `Chhattisgarh is a state in central India known for its rich cultural heritage and natural beauty.
 
       One of the most popular dishes in Chhattisgarh is Muthiya, a traditional dish made from grated lauki is mixed with chickpea flour and spices. Then is it steamed with mustard seeds and curry leaves. Like a lot of Indian food, it is relatively healthy and nutritous..
@@ -185,7 +185,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/chana-samosa-recipe/`
     },
     'Odisha': {
-      images: ['../src/images/odisha.jpg'],
+      images: ['/images/Odisha.jpg'],
       description: `Odisha is a state in eastern India known for its ancient temples and tribal cultures.
 
       One of the most popular dishes in Odisha is Chhena Poda which is a dessert made from fresh paneer (similar to tofu, but made with milk), sugar, and cardamom. The dish is baked until it is golden and has a caramelized crust. 
@@ -197,7 +197,7 @@ const StatePage = () => {
 
     },
     'West Bengal': {
-      images: ['../src/images/west_bengal.jpg'],
+      images: ['/images/West_bengal.jpg'],
       description: `West Bengal is a state in eastern India known for its rich cultural heritage and literature.
 
       One of the most popular dishes in West Bengal is Macher Jhol (fish curry). It is a traditional dish made with fish, potatoes, tomatoes, and a mix of spices.
@@ -209,7 +209,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/macher-jhol-recipe/`
     },
     'Jharkhand': {
-      images: ['../src/images/jharkhand.jpg'],
+      images: ['/images/Jharkhand.jpg'],
       description: `Jharkhand is a state in eastern India known for its hills, waterfalls, and vast mineral resources.
 
       One of the most popular dishes is Thekua, a traditional sweet snack made from wheat flour, jaggery (cane sugar), and coconut. It is deep fried and has a crunchy texture and the flavor is sweet and earthy.
@@ -221,7 +221,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/thekua-recipe/`
     },
     'Bihar': {
-      images: ['../src/images/bihar.jpg'],
+      images: ['/images/Bihar.jpg'],
       description: `Bihar is a state in eastern India known for its ancient history, monuments, and heritage.
 
       One of the most popular dishes in Bihar is Litti Chokha, a traditional dish made from wheat flour dough filled with roasted gram flour and spices, served with mashed vegetables.
@@ -233,7 +233,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/litti-chokha-recipe/`
     },
     'Uttarakhand': {
-      images: ['../src/images/uttarakhand.jpg'],
+      images: ['/images/Uttarakhand.jpg'],
       description: `Uttarakhand is a state in northern India known for its scenic beauty, including the Himalayas, forests, wildlife, and rivers.
 
       One of the most popular dishes in Uttarakhand is Kafuli, a traditional dish made from green leafy vegetables like spinach and fenugreek, cooked with rice or wheat flour and seasoned with spices. It is a nutritious and flavorful dish that is often enjoyed during the winter months. It is really good for you and can help you lose weight whole also keeping you fuull
@@ -245,7 +245,7 @@ const StatePage = () => {
       https://www.vegrecipesofindia.com/kafuli-recipe/`
     },
     'Himachal Pradesh': {
-      images: ['../src/images/himachal_pradesh.jpg'],
+      images: ['/images/Himachal_pradesh.jpg'],
       description: `Himachal Pradesh is a beautifal state in northern India known for its scenic mountain towns and resorts.
 
       One of the most popular dishes in Himachal Pradesh is Dham, a traditional feast that includes rice, dal, rajma, curd, and sweet dishes. It is usually served during festivals and special occasions.
