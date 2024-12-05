9a0e42b5e0a7ffbf955544a66bf5e84e6de61342
Maahir Vohra <117885945+Mxxhxr@users.noreply.github.com>
2024-12-05 15:49:45 -0500

    vite updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/hss_project/', // Match this to your repository name
+// });
+
 export default defineConfig({
   plugins: [react()],
-  base: '/hss_project/', // Match this to your repository name
+  // base: '/hss_project/', // This must match your repo name
 });
