9601a7879b3b6541754f6445e89c26c0d94fb1db
Maahir Vohra <117885945+Mxxhxr@users.noreply.github.com>
2024-12-04 22:40:50 -0500

    updated config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/hss_project/', // Set to the repository name for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
